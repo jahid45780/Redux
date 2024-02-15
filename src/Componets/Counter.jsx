f73fe52a924e20ab98315248aab491d4da9d9273
jahid45780 <jahidhossain88899@gmail.com>
2024-02-16 00:35:17 +0600

    add the h

--- src/Componets/Counter.jsx
+++ src/Componets/Counter.jsx
@@ -1,9 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByValue } from "../Redux/Features/Counter/CounterSlice";
 
 
 const Counter = () => {
+
+const {count} = useSelector((state) => state.counter)
+const dispatch = useDispatch()
+
     return (
         <div>
-            
+            <button onClick={()=>dispatch(incrementByValue(5))} >increment by 5 </button> <br />
+            <button onClick={()=>dispatch(increment())} >increment  </button>
+            <div> 
+                <h1> {count} </h1>
+            </div>
+            <button onClick={()=>dispatch(decrement())} > decrement </button>
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import Counter from "../Componets/Counter";
 
 
 const Home = () => {
     return (
         <div>
             <h1> home</h1>
+            <Counter></Counter>
         </div>
     );
 };
