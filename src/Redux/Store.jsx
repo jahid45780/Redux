2edd06bf5b8a82b94d145dcc051c14b8b2c0261b
jahid45780 <jahidhossain88899@gmail.com>
2024-02-15 21:37:07 +0600

    first commit

---
+++ src/Redux/Store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import CounterReducer from './Features/Counter/CounterSlice'
+
+const store = configureStore({
+     reducer:{
+         counter: CounterReducer
+     },
+     
+})
+
+export default store
